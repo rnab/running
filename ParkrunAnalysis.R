b9c69013ec8d3585271ecb759d73fa4a179b488d
R N <riknabeshima@gmail.com>
2019-06-10 22:28:59 +0100

    minor updates to parkrun analysis

--- ParkrunAnalysis.R
+++ ParkrunAnalysis.R
@@ -1,13 +1,14 @@
-setwd("C:/Users/rikna/OneDrive/Documents/Running")
-
-
-
+#Read in the results of one Burgess parkrun
+library(ggplot2)
 pr<-read.csv("prres.csv")
-
 
 #rn best time 22.12
 rnpb=22.12*60
-library(ggplot2)
-ggplot(data=pr[pr$sex!='0'&pr$sex!='',],aes(x=s))+geom_histogram()+facet_grid(sex~.)+ggtitle("Distribution of male vs. female times in Burgess parkrun")+
-  geom_vline(xintercept=rnpb)
+
+ggplot(data=pr[pr$sex!='0'&pr$sex!='',],aes(x=s))+
+  geom_histogram()+
+  facet_grid(sex~.)+
+  ggtitle("Distribution of male vs. female times in Burgess parkrun")+
+  geom_vline(xintercept=rnpb)+
+  xlab("Time (s)")+ylab("Number of runners")+theme_bw()
 
